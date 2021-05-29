b70b28ad574bf68bb8de2803b47b183596dd9f65
Ubuntu <ubuntu@ip-172-31-33-189.ap-south-1.compute.internal>
2021-05-29 07:18:28 +0000

    updates from ec2 instance

--- wardhacovidhelp/settings.py
+++ wardhacovidhelp/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'wardhacovidhelp.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(BASE_DIR / 'db.sqlite3'),
+        'NAME': BASE_DIR / 'db.sqlite3'
     }
 }
 
@@ -120,9 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+#STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, "static"),
+#]
+STATIC_ROOT = os.path.join(BASE_DIR,"static")
 MEDIA_URL = "/images/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "static/images")
